9e6d5752c3cc2ba0c7552ccff03df768d1a61c74
Prince9555 <131245773+Prince9555@users.noreply.github.com>
2023-06-25 21:07:02 +0530

    Add files via upload

---
+++ day49.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int z = x ^ y;
+        int res = 0;
+        while (z > 0) {
+            res++;
+            z = z & (z-1);
+        }
+        return res;
+    }
+}
